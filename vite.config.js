b0499c75c5cc44fb2457a958361fb62dfd1e03f9
Charlieee1 <77288859+Charlieee1@users.noreply.github.com>
2025-04-07 17:45:04 -0400

    making it work

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vite.dev/config/
 export default defineConfig({
