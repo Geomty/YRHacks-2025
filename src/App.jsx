c6629ae2f8296499516d6dc4b5888018cc5dffa1
C-stands-for-code <115311413+Len-is-coding@users.noreply.github.com>
2025-04-07 18:29:14 -0400

    deleted default template stuff

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,38 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  form.addEventListener(`focus`, () => form.select());
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className ="App">
+      <label>
+          <input value="untitled root" className="form"/>
+      </label>
+
+      <div className="prompt-window">
+        <div className="embed-window">
+            insert website
+        </div>
+
+        <label>
+          <input value="Type here to search..." id="browseFor" className="form"/>
+        </label>
+
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+  
+    </div>
   )
 }
 
